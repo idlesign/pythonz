c1555b226c9d53094c88da122c23867049639e52
idle sign <idlesign@yandex.ru>
2014-09-25 21:14:21 +0700

    Отображаем время в зависимости от часового пояса из профиля пользователя.

--- apps/middleware.py
+++ apps/middleware.py
@@ -1,10 +1,20 @@
 from django.utils import timezone
+from pytz import UnknownTimeZoneError
 
 
 class TimezoneMiddleware(object):
     """Устанавливает текущую временную зону."""
 
     def process_request(self, request):
-        # Пока будем считать, что Нск - пуп Земли.
-        # TODO При случае что-нибудь придумать. Возможно pysyge.
-        timezone.activate('Asia/Novosibirsk')
+        default_timezone = 'Asia/Novosibirsk'
+        current_timezone = default_timezone
+
+        user = getattr(request, 'user', None)
+        if user is not None and user.is_authenticated:
+            if user.timezone:
+                current_timezone = user.timezone
+
+        try:
+            timezone.activate(current_timezone)
+        except UnknownTimeZoneError:
+            timezone.activate(default_timezone)
