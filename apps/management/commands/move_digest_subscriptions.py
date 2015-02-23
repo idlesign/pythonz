48b7fdc5a19654bf94dec735ba65b11e5f7dd836
idle sign <idlesign@yandex.ru>
2015-02-23 18:23:12 +0600

    Добавлена команда для миграции подписок на новый sitemessage.

---
+++ apps/management/commands/move_digest_subscriptions.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from sitemessage.models import Subscription
+
+from ...models import User
+
+
+class Command(BaseCommand):
+
+    help = 'Migrates digest subscriptions handling from pythonz to sitemessages'
+
+    def handle(self, *args, **options):
+
+        self.stdout.write('Starting migration ...\n')
+        try:
+            subscribers = User.objects.filter(digest_enabled=True).values_list('id', flat=True)
+            targets = []
+            for subscriber in subscribers:
+                targets.append(Subscription(
+                    message_cls='digest',
+                    message_cls='smtp',
+                    recipient_id=subscriber
+                ))
+            Subscription.objects.bulk_create(targets)
+
+        except Exception as e:
+            self.stderr.write(self.style.ERROR('Migration failed: %s\n' % e))
+
+        else:
+            self.stdout.write('All is done.\n')
