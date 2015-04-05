d58caf491e5d2bc82ecedeea9196c2944dad7c1b
idle sign <idlesign@yandex.ru>
2015-04-05 19:27:05 +0600

    Исправлено ошибка при получении rss.

--- apps/generics/realms.py
+++ apps/generics/realms.py
@@ -75,7 +75,7 @@ class RealmBase(object):
     view_details = None
     view_details_base_class = DetailsView
     view_details_url = r'^(?P<obj_id>\d+)/$'
-    view_details_slug_url = r'^(?P<obj_id>[A-z\.-]+)/$'
+    view_details_slug_url = r'^(?P<obj_id>(?!feed)[A-z\.-]+)/$'
 
     # Представление для добавления нового элемента.
     view_add = None
