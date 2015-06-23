8da7e5fcd10e6eccbd4d39cf07186e65851dec15
idle sign <idlesign@yandex.ru>
2015-06-23 20:37:51 +0600

    Обновлены переводы дзена.

--- apps/zen.py
+++ apps/zen.py
@@ -9,8 +9,8 @@ ZEN = (
     ('Simple is better than complex.', 'Простое лучше сложного.'),
     ('Complex is better than complicated.', 'Сложное лучше мудрёного.'),
     ('Flat is better than nested.', 'Плоское лучше вложенного.'),
-    ('Sparse is better than dense.', 'Лучше редко, чем кучно.'),
-    ('Readability counts.', 'Читабельность всчёт.'),
+    ('Sparse is better than dense.', 'Разреженное лучше плотного.'),
+    ('Readability counts.', 'Читабельность важна.'),
     ('Special cases aren\'t special enough to break the rules.\nAlthough practicality beats purity.',
      'Исключения недостаточно исключительны, чтобы нарушать правила.\nХотя, практичность превыше чистоты.'),
     ('Errors should never pass silently.\nUnless explicitly silenced.',
@@ -19,16 +19,16 @@ ZEN = (
      'Пред лицом многозначительности презри желание догадаться.'),
     ('There should be one — and preferably only one — obvious way to do it.\n'
      'Although that way may not be obvious at first unless you\'re Dutch.',
-     'Должен быть один —  и лучше единственный — очевидный способ достичь цели.\n'
-     'Хотя, этот способ поначалу может казаться неочевидным, если вы не голландец.'),
+     'Должен быть один — и лучше единственный — очевидный способ достичь цели.\n'
+     'Впрочем, если вы не голландец, способ этот поначалу может казаться неочевидным.'),
     ('Now is better than never.\nAlthough never is often better than <em>right</em> now.',
-     'Лучше теперь, чем никогда.\nХотя, часто никогда, лучше чем <em>сию минуту</em>.'),
+     'Лучше сейчас, чем никогда.\nХотя, часто никогда лучше, чем <em>прямо</em> сейчас.'),
     ('If the implementation is hard to explain, it\'s a bad idea.',
      'Если реализацию трудно описать, значит идея была некудышной.'),
     ('If the implementation is easy to explain, it may be a good idea.',
      'Если реализацию легко описать — возможно, идея была хорошей.'),
     ('Namespaces are one honking great idea — let\'s do more of those!',
-     'Пространства имён были запредельно замечательной идеей — так понаделаем их ещё!'),
+     'Пространства имён были блестящей идеей — генерируем ещё!'),
 )
 
 register_dynamic_block('zen', lambda **kwargs: choice(ZEN))
