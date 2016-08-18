797962b4dcf3e7ee9b7c19cea63d142a154205dc
idle sign <idlesign@yandex.ru>
2016-08-18 10:36:22 +0700

    Устранено предупреждение robots.

--- urls.py
+++ urls.py
@@ -1,14 +1,15 @@
 from sys import argv
 
-from siteprefs.toolbox import autodiscover_siteprefs
-from sitegate.toolbox import get_sitegate_urls
-from sitemessage.toolbox import get_sitemessage_urls
 from django.conf.urls import include, url
 from django.shortcuts import render
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import logout
+from siteprefs.toolbox import autodiscover_siteprefs
+from sitegate.toolbox import get_sitegate_urls
+from sitemessage.toolbox import get_sitemessage_urls
+from robots.views import rules_list
 
 from apps.realms import bootstrap_realms  # Здесь относительный импорт работать не будет.
 from apps.views import page_not_found, permission_denied, server_error, index, search, login, telebot
@@ -23,7 +24,7 @@ urlpatterns = [
     url(r'^promo/$', render, {'template_name': 'static/promo.html'}),
     url(r'^about/$', render, {'template_name': 'static/about.html'}),
     url(r'^sitemap/$', render, {'template_name': 'static/sitemap.html'}),
-    url(r'^robots\.txt$', include('robots.urls')),
+    url(r'^robots\.txt', rules_list, name='robots_rule_list'),
     url(r'^%s/' % settings.TELEGRAM_BOT_URL, telebot),
     url(r'^%s/' % settings.ADMIN_URL, include(admin.site.urls)),
 ]
