eddd9b17b27311f0db414b5c56326fcb672f0947
idle sign <idlesign@yandex.ru>
2016-09-21 19:24:59 +0700

    sync_peps ознакомлена с имеющимися URL.

--- apps/management/commands/sync_peps.py
+++ apps/management/commands/sync_peps.py
@@ -1,6 +1,11 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
 from ...models import PEP
+
+#  PEP.sync_from_repository() может вызывать систему оповещений,
+# для которой необходимо, чтобы работала reverse(). Подгружаем данные о доступных URL.
+__import__(settings.ROOT_URLCONF)
 
 
 class Command(BaseCommand):
