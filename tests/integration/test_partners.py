30a8386484c196207ff04a70b4e1290465b9fd35
Ruslan Nasonov <rus.nasonov@gmail.com>
2017-05-31 16:37:24 +0700

    добавлены тесты BooksRu
    pytest-mock нужен для того, чтобы не воспроизводить ненужные в тесте объекты,
    в данном случае realm

---
+++ tests/integration/test_partners.py
@@ -0,0 +1,21 @@
+def test_booksru(mocker):
+    from apps.integration import partners
+    from apps.models import PartnerLink
+
+    book_link = 'https://www.books.ru/books/nosql-novaya-metodologiya-razrabotki-nerelyatsionnykh-baz-dannykh-3195898'
+    partner_id = '12345'
+    booksru = partners.BooksRu(partner_id=partner_id)
+    link = PartnerLink(url=book_link)
+    mock_realm = mocker.Mock()
+
+    data = booksru.get_link_data(mock_realm, link)
+
+    assert "руб" in data['price']
+    assert "%s?partner=%s" % (book_link, partner_id) == data['url']
+    assert 'https://www.books.ru/static/images/favicon.ico' == data['icon_url']
+
+    # проверка валюты нидерландов
+    partners.BooksRu.default_location = 'ned'
+    data = booksru.get_link_data(mock_realm, link)
+
+    assert "euro" in data['price']
