f88f60b40ac02ca266f6227155835cc0be8333d1
idle sign <idlesign@yandex.ru>
2017-06-11 21:52:52 +0700

    Обучение кодировщика json использованию методов to_json.

--- apps/__init__.py
+++ apps/__init__.py
@@ -2,3 +2,21 @@ VERSION = (0, 29, 0)
 
 # Конфигурация лежит здесь:
 default_app_config = 'apps.config.PythonzAppsConfig'
+
+
+from json import JSONEncoder
+
+
+def patch_json_encoder():
+    """Модифицирует базовый json-кодировщик, для поддежки метода
+    to_json пользовательских типов.
+
+    """
+    def json_encoder_default(self, obj):
+        return getattr(obj.__class__, 'to_json', json_encoder_default.default_)(obj)
+
+    json_encoder_default.default_ = JSONEncoder().default
+    JSONEncoder.default = json_encoder_default
+
+
+patch_json_encoder()
