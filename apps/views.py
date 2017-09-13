8468dfe51ca8558a1ca45dcf4a84d522836e9a7a
idle sign <idlesign@yandex.ru>
2017-09-13 22:43:16 +0700

    Изменения для директа.

--- apps/views.py
+++ apps/views.py
@@ -263,7 +263,7 @@ def search(request):
     Если найден один результат, перенаправляет на страницу результата.
 
     """
-    search_term = request.GET.get('search_term', '').strip(' ()')
+    search_term = request.GET.get('text', '').strip(' ()')
 
     if not search_term:
         return redirect('index')
