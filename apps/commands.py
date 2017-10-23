0c6b5de46b15ebcb5b6f94c787ab48a7cc0d8711
idle sign <idlesign@yandex.ru>
2017-10-23 20:50:29 +0700

    Выделен модуль для команд общего толка.

---
+++ apps/commands.py
@@ -0,0 +1,40 @@
+from datetime import timedelta
+
+from collections import defaultdict
+from django.utils import timezone
+
+from .realms import get_realms
+from .generics.models import RealmBaseModel
+
+
+def publish_postponed():
+    """Публикует материалы, назначенные к отложенной публикации."""
+    status_postponed = RealmBaseModel.STATUS_POSTPONED
+    status_published = RealmBaseModel.STATUS_PUBLISHED
+
+    # В каждой области будем публиковать отложенные материалы,
+    # если данный автор не публиковался более узананного периода.
+    date_before = timezone.now() - timedelta(hours=22)
+
+    for realm in get_realms().values():
+        realm_model = realm.model
+        postponed_by_submitter = defaultdict(list)
+
+        qs = realm_model.objects.filter(status=status_postponed).order_by('time_created')
+        for item in qs:
+            submitter_id = getattr(item, 'submitter_id', None)
+            if submitter_id is None:
+                continue
+            postponed_by_submitter[submitter_id].append(item)
+
+        for submitter_id, items in postponed_by_submitter.items():
+            latest = realm_model.objects.filter(
+                status=status_published, submitter_id=submitter_id,
+            ).latest('time_published')
+
+            if not latest or latest.time_published < date_before:
+                # Пока публикуем только первый из назначенных к публикации материалов.
+                # В последующем возможно логика будет усложнена.
+                item = items[0]
+                item.mark_published()
+                item.save()
--- apps/management/commands/publish_postponed.py
+++ apps/management/commands/publish_postponed.py
@@ -1,12 +1,6 @@
-from datetime import timedelta
-
-from collections import defaultdict
-
 from django.core.management.base import BaseCommand
-from django.utils import timezone
 
-from ...realms import get_realms
-from ...generics.models import RealmBaseModel
+from ...commands import publish_postponed
 
 
 class Command(BaseCommand):
@@ -17,34 +11,6 @@ class Command(BaseCommand):
 
         self.stdout.write('Postponed publishing started ...\n')
 
-        status_postponed = RealmBaseModel.STATUS_POSTPONED
-        status_published = RealmBaseModel.STATUS_PUBLISHED
-
-        # В каждой области будем публиковать отложенные материалы,
-        # если данный автор не публиковался более узананного периода.
-        date_before = timezone.now() - timedelta(hours=22)
-
-        for realm in get_realms().values():
-            realm_model = realm.model
-            postponed_by_submitter = defaultdict(list)
-
-            qs = realm_model.objects.filter(status=status_postponed).order_by('time_created')
-            for item in qs:
-                submitter_id = getattr(item, 'submitter_id', None)
-                if submitter_id is None:
-                    continue
-                postponed_by_submitter[submitter_id].append(item)
-
-            for submitter_id, items in postponed_by_submitter.items():
-                latest = realm_model.objects.filter(
-                    status=status_published, submitter_id=submitter_id,
-                ).latest('time_published')
-
-                if not latest or latest.time_published < date_before:
-                    # Пока публикуем только первый из назначенных к публикации материалов.
-                    # В последующем возможно логика будет усложнена.
-                    item = items[0]
-                    item.mark_published()
-                    item.save()
+        publish_postponed()
 
         self.stdout.write('Postponed publishing finished.\n')
