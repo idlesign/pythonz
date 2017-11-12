81c461a1c1de3d238e5aa6aeca9584a8d9d7714e
idle sign <idlesign@yandex.ru>
2017-11-12 10:44:35 +0700

    Задания переложены на наробтников.

--- apps/tasks.py
+++ apps/tasks.py
@@ -6,32 +6,32 @@ from .models import Summary, PEP, ExternalResource, Vacancy
 from .sitemessages import PythonzEmailDigest
 
 
-@register_timer(60)
+@register_timer(60, signal_or_target='worker')
 def task_send_messages(sig_num):
     """Отправка оповещений."""
     send_scheduled_messages(priority=1)
 
 
-@register_cron(hour=-4, minute=30)
+@register_cron(hour=-4, minute=30, signal_or_target='worker')
 def task_get_vacancies(sig_num):
     """Синхронизация вакансий."""
     Vacancy.update_statuses()
     Vacancy.fetch_new()
 
 
-@register_cron(hour=-2, minute=1)
+@register_cron(hour=-2, minute=1, signal_or_target='worker')
 def task_get_resources(sig_num):
     """Подтягивание данных из внешних ресурсов."""
     ExternalResource.fetch_new()
 
 
-@register_cron(hour=-1, minute=10)
+@register_cron(hour=-1, minute=10, signal_or_target='worker')
 def task_publish_postponed(sig_num):
     """Публикация отложенных материалов."""
     publish_postponed()
 
 
-@register_cron(weekday=0, hour=7, minute=10)
+@register_cron(weekday=0, hour=7, minute=10, signal_or_target='worker')
 def task_create_summary(sig_num):
     """Еженедельная статья-сводка.
     Воскресенье 14:10 Нск.
@@ -40,7 +40,7 @@ def task_create_summary(sig_num):
     Summary.create_article()
 
 
-@register_cron(weekday=4, hour=1, minute=40)
+@register_cron(weekday=4, hour=1, minute=40, signal_or_target='worker')
 def task_sync_peps(sig_num):
     """Синхронизация данных PEP.
     Четверг 06:40 Нск.
@@ -49,7 +49,7 @@ def task_sync_peps(sig_num):
     PEP.sync_from_repository()
 
 
-@register_cron(weekday=5, hour=3, minute=40)
+@register_cron(weekday=5, hour=3, minute=40, signal_or_target='worker')
 def task_digest_create(sig_num):
     """Компиляция еженедельного дайджеста.
     Пятница 08:40 Нск.
@@ -58,7 +58,7 @@ def task_digest_create(sig_num):
     PythonzEmailDigest.create()
 
 
-@register_cron(weekday=5, hour=4, minute=0)
+@register_cron(weekday=5, hour=4, minute=0, signal_or_target='worker')
 def task_digest_send(sig_num):
     """Рассылка еженедельного дайджеста.
     Пятница 09:00 Нск.
