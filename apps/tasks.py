d8617898b996fda57fe4d641ed8d26c549edb3a5
idle sign <idlesign@yandex.ru>
2017-12-16 12:24:28 +0700

    Время оповещений сдвинуто ближе к европейской части страны.

--- apps/tasks.py
+++ apps/tasks.py
@@ -25,13 +25,13 @@ def task_get_resources(sig_num):
     ExternalResource.fetch_new()
 
 
-@register_cron(hour=-1, minute=10, signal_or_target='worker')
+@register_cron(hour=-2, minute=15, signal_or_target='worker')
 def task_publish_postponed(sig_num):
     """Публикация отложенных материалов."""
     publish_postponed()
 
 
-@register_cron(weekday=0, hour=7, minute=10, signal_or_target='worker')
+@register_cron(weekday=0, hour=8, minute=10, signal_or_target='worker')
 def task_create_summary(sig_num):
     """Еженедельная статья-сводка.
     Воскресенье 14:10 Нск.
@@ -40,28 +40,28 @@ def task_create_summary(sig_num):
     Summary.create_article()
 
 
-@register_cron(weekday=4, hour=1, minute=40, signal_or_target='worker')
+@register_cron(weekday=5, hour=5, minute=20, signal_or_target='worker')
 def task_sync_peps(sig_num):
     """Синхронизация данных PEP.
-    Четверг 06:40 Нск.
+    Пятница 11:20 Нск.
 
     """
     PEP.sync_from_repository()
 
 
-@register_cron(weekday=5, hour=3, minute=40, signal_or_target='worker')
+@register_cron(weekday=5, hour=5, minute=40, signal_or_target='worker')
 def task_digest_create(sig_num):
     """Компиляция еженедельного дайджеста.
-    Пятница 08:40 Нск.
+    Пятница 11:40 Нск.
 
     """
     PythonzEmailDigest.create()
 
 
-@register_cron(weekday=5, hour=4, minute=0, signal_or_target='worker')
+@register_cron(weekday=5, hour=6, minute=0, signal_or_target='worker')
 def task_digest_send(sig_num):
     """Рассылка еженедельного дайджеста.
-    Пятница 09:00 Нск.
+    Пятница 12:00 Нск.
 
     """
     send_scheduled_messages(priority=7)
