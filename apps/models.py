a75797b1353bb3a269d24ebfb37c14c6f3b9ba49
idle sign <idlesign@yandex.ru>
2018-01-21 12:19:53 +0700

    Облегчена страница со списком версий.

--- apps/models.py
+++ apps/models.py
@@ -971,7 +971,7 @@ class Version(InheritedModel, RealmBaseModel, CommonEntityModel, ModelWithDiscus
         ordering = ('-date',)
 
     autogenerate_slug = True
-    items_per_page = 15
+    items_per_page = 10
 
     def __str__(self):
         return 'Python %s' % self.title
