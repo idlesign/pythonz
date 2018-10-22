a64e11ebf95a05acc5259681dbefe62f65970bc9
idle sign <idlesign@yandex.ru>
2018-10-22 19:13:03 +0700

    Не храним кеш в режиме разработки.

--- settings/settings_development.py
+++ settings/settings_development.py
@@ -15,6 +15,11 @@ TEMPLATES[0]['OPTIONS']['debug'] = True
 INSTALLED_APPS.append('debug_toolbar')
 MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
 
 LOGGERS.update({
 
