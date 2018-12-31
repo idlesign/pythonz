13b2da9b4b11a1633016e8cdc55656fdf39f118a
idle sign <idlesign@yandex.ru>
2018-12-31 12:39:59 +0700

    Добавлены базовые setup файлы.

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+This packages is to be installed with
+
+    pip install -e .
+
+"""
+import sys
+
+from setuptools import setup
+
+PYTEST_RUNNER = ['pytest-runner'] if 'test' in sys.argv else []
+
+setup(
+    name='pythonz',
+
+    url='https://github.com/idlesign/pythonz',
+
+    author='Igor `idle sign` Starikov',
+    author_email='idlesign@yandex.ru',
+
+    tests_require=[
+        'pytest',
+        'pytest-djangoapp>=0.11.0'
+    ],
+    test_suite='tests',
+    setup_requires=[] + PYTEST_RUNNER,
+
+    entry_points={
+        'console_scripts': ['pythonz = manage:manage'],
+    },
+
+)
