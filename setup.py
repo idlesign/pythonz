7f294dbdb30184513e9345dde56da0f5c8af1b10
idle sign <idlesign@yandex.ru>
2020-01-06 10:50:09 +0700

    Адаптируем setup.py.

--- setup.py
+++ setup.py
@@ -4,29 +4,57 @@ This packages is to be installed with
     pip install -e .
 
 """
+import io
+import os
+import re
 import sys
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-PYTEST_RUNNER = ['pytest-runner'] if 'test' in sys.argv else []
+PATH_BASE = os.path.dirname(__file__)
+
+
+def read_file(fpath):
+    """Reads a file within package directories."""
+    with io.open(os.path.join(PATH_BASE, fpath)) as f:
+        return f.read()
+
+
+def get_version():
+    """Returns version number, without module import (which can lead to ImportError
+    if some dependencies are unavailable before install."""
+    contents = read_file(os.path.join('pythonz', '__init__.py'))
+    version = re.search('VERSION = \(([^)]+)\)', contents)
+    version = version.group(1).replace(', ', '.').strip()
+    return version
+
 
 setup(
     name='pythonz',
-
+    version=get_version(),
     url='https://github.com/idlesign/pythonz',
+
+    description='Source code for pythonz.net',
+    long_description=read_file('README.rst'),
 
     author='Igor `idle sign` Starikov',
     author_email='idlesign@yandex.ru',
 
-    tests_require=[
-        'pytest',
-        'pytest-djangoapp>=0.13.0'
-    ],
-    test_suite='tests',
-    setup_requires=[] + PYTEST_RUNNER,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=[],
+    setup_requires=[] + (['pytest-runner'] if 'test' in sys.argv else []) + [],
 
     entry_points={
-        'console_scripts': ['pythonz = manage:manage'],
+        'console_scripts': ['pythonz = pythonz.manage:main'],
     },
 
+    test_suite='tests',
+
+    tests_require=[
+        'pytest',
+        'pytest-djangoapp>=0.13.0',
+    ],
 )
