f0ece3dbbf51d4034dd8f3f3c05486adb79a8cc1
idle sign <idlesign@yandex.ru>
2020-01-20 20:18:26 +0700

    Показываем некоторые параметры конфигурации в админке.

---
+++ pythonz/apps/settings.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+
+
+SOCKS5_PROXY = settings.SOCKS5_PROXY
+
+GOOGLE_API_KEY = settings.GOOGLE_API_KEY
+YANDEX_SEARCH_ID = settings.YANDEX_SEARCH_ID
+
+TELEGRAM_BOT_TOKEN = settings.TELEGRAM_BOT_TOKEN
+TELEGRAM_GROUP = settings.TELEGRAM_GROUP
+
+VK_ACCESS_TOKEN = settings.VK_ACCESS_TOKEN
+VK_GROUP = settings.VK_GROUP
+
+
+if 'siteprefs' in settings.INSTALLED_APPS:
+
+    from siteprefs.toolbox import preferences
+
+    with preferences() as prefs:
+
+        prefs(
+            SOCKS5_PROXY,
+            GOOGLE_API_KEY,
+            YANDEX_SEARCH_ID,
+            TELEGRAM_BOT_TOKEN,
+            TELEGRAM_GROUP,
+            VK_ACCESS_TOKEN,
+            VK_GROUP,
+        )
