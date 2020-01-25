037ab7ff6a01981e5796479a0c3ae9a744411764
idle sign <idlesign@yandex.ru>
2020-01-25 14:37:41 +0700

    Актуализированы тесты для интеграций.

--- tests/integration/test_summary.py
+++ tests/integration/test_summary.py
@@ -2,66 +2,111 @@ from datetime import datetime
 
 import pytest
 
-from pythonz.apps.integration.summary import MailarchConferences, GithubTrending, Stackoverflow, Lwn
+from pythonz.apps.integration.summary import MailarchConferences, GithubTrending, Stackoverflow, Lwn, MailarchDev
+
+pytestmark = [pytest.mark.slow]
 
 
 def test_lwn():
-    results, latest = Lwn(None, None).run()
+
+    results, latest = Lwn(
+        previous_result=[],
+        previous_dt=None
+    ).run()
 
     assert isinstance(results, list)
     assert isinstance(latest, dict)
     assert results
 
-    results, latest = Lwn(latest, None).run()
+    results, latest = Lwn(
+        previous_result=latest,
+        previous_dt=None
+    ).run()
     assert isinstance(results, list)
     assert isinstance(latest, dict)
     assert not results
 
 
-@pytest.mark.skip('Долгий')
 def test_pipermail():
-    by_title, latest = MailarchConferences(None, None, year_month='2009-May').run()
 
-    assert len(by_title) == 7
-    assert not by_title.popitem()[0].startswith('[')
+    current, latest = MailarchConferences(
+        previous_result=[],
+        previous_dt=None,
+        year_month='2009-May'
+    ).run()
+
+    assert len(current) == 7
+    assert not current[0].title.startswith('[')
     assert len(latest) == 1
     assert latest[0] == 'https://mail.python.org/pipermail/conferences/2009-May/000016.html'
 
     prev_result = ['https://mail.python.org/pipermail/conferences/2009-May/000009.html']
-    by_title, latest = MailarchConferences(prev_result, None, year_month='2009-May').run()
 
-    assert len(by_title) == 4
+    current, latest = MailarchConferences(
+        previous_result=prev_result,
+        previous_dt=None,
+        year_month='2009-May'
+    ).run()
+
+    assert len(current) == 4
     assert len(latest) == 1
 
-    MailarchConferences(['faked'], None).run()
+    MailarchConferences(
+        previous_result=['faked'],
+        previous_dt=None
+    ).run()
 
     assert len(latest) == 1
 
 
-@pytest.mark.skip('Долгий')
+def test_hyperkitty():
+
+    current, latest = MailarchDev(
+        previous_result=[],
+        previous_dt=datetime(2019, 12, 31),
+        till=datetime(2019, 12, 31)
+    ).run()
+
+    assert len(current) == 6
+    assert len(latest) == 6
+
+
 def test_github():
-    by_title, latest = GithubTrending(None, None).run()
-    len_latest = len(latest)
-    len_by_title = len(by_title)
 
-    assert len_latest == len_by_title
+    current, latest = GithubTrending(
+        previous_result=[],
+        previous_dt=None
+    ).run()
+
+    len_latest = len(latest)
+    len_current = len(current)
+
+    assert len_latest == len_current
 
     one_latest = latest.pop()
 
-    by_title, latest = GithubTrending(latest, None).run()
+    current, latest = GithubTrending(
+        previous_result=latest,
+        previous_dt=None
+    ).run()
 
-    assert len(by_title) == 1
+    assert len(current) == 1
     assert len(latest) == 1
     assert one_latest in latest
 
 
-@pytest.mark.skip('Долгий')
 def test_stack():
 
-    by_title, latest = Stackoverflow(None, None).run()
+    current, latest = Stackoverflow(
+        previous_result=[],
+        previous_dt=None
+    ).run()
 
     assert len(latest)
 
-    by_title, latest = Stackoverflow(None, previous_dt=datetime.now()).run()
+    current, latest = Stackoverflow(
+        previous_result=[],
+        previous_dt=datetime.now()
+    ).run()
 
     assert not len(latest)  # за сегодня ещё нет вопросов
