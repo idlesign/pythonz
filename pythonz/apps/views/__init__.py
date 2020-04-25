f0d57930bcc9dca9c5e1634609f11cede805a8a9
idle sign <idlesign@yandex.ru>
2020-04-25 09:29:46 +0700

    Чистка представлений.

---
+++ pythonz/apps/views/__init__.py
@@ -0,0 +1,13 @@
+from .basic import login, permission_denied, page_not_found, server_error
+from .callback import telebot
+from .categories import CategoryListingView
+from .ide import ide
+from .index import index
+from .peps import PepListingView
+from .persons import PersonDetailsView
+from .places import PlaceDetailsView, PlaceListingView
+from .references import ReferenceDetailsView, ReferenceListingView
+from .search import search
+from .users import UserDetailsView, UserEditView, user_settings
+from .vacancies import VacancyListingView
+from .versions import VersionDetailsView
