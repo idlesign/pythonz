4971c5b82220f9392aa9110af73df40883bce959
idle sign <idlesign@yandex.ru>
2020-06-24 19:22:46 +0700

    Дозволяем сброс паролей пользователям.

--- pythonz/urls.py
+++ pythonz/urls.py
@@ -3,9 +3,11 @@ from sys import argv
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import (
+    LogoutView, PasswordResetConfirmView, PasswordResetDoneView, PasswordResetView, PasswordResetCompleteView
+)
 from django.shortcuts import render
-from django.urls import path
+from django.urls import path, include
 from robots.views import rules_list
 from sitegate.toolbox import get_sitegate_urls
 from sitemessage.toolbox import get_sitemessage_urls
@@ -13,10 +15,18 @@ from sitemessage.toolbox import get_sitemessage_urls
 from .apps.realms import bootstrap_realms
 from .apps.views import page_not_found, permission_denied, server_error, index, search, login, telebot, user_settings
 
+urls_password_reset = [
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
+
 urlpatterns = [
     path('', index, name='index'),
     path('search/site/', render, {'template_name': 'static/search_site.html'}, name='search_site'),
     path('search/', search, name='search'),
+    path('auth/', include(urls_password_reset)),
     path('login/', login, name='login'),
     path('settings/', user_settings, name='settings'),
     path('logout/', LogoutView.as_view(), {'next_page': '/'}, name='logout'),
