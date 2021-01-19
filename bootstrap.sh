69cf1f26096e6b44cf8d6c125275542455fd91b5
idle sign <idlesign@yandex.ru>
2021-01-19 21:00:09 +0700

    Добавлен скрипт, упрощаяющий подготовку окрудения для локальной разработки.

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Bootstraps a local development environment.
+#
+# Run as follows:
+#   ./bootstrap.sh
+#
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install -r requirements.txt
+pip install -r tests/requirements.txt
+pip install -e .
+
+mkdir state
+
+pythonz migrate
+pythonz createsuperuser
