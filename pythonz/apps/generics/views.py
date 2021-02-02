6c5bb5400e44938b833d3a62a3b4421748e6c5bd
idle sign <idlesign@yandex.ru>
2021-02-02 19:33:47 +0700

    Сокращаем количество выводимых ссылок на категории.

--- pythonz/apps/generics/views.py
+++ pythonz/apps/generics/views.py
@@ -240,9 +240,10 @@ class ListingView(RealmView):
 
         categories = [
             Category(id=category_id, title=category_title)
-            for category_id, category_title in Tie.objects.filter(
-                content_type=content_type,
-            ).values_list('category_id', 'category__title').distinct()
+            for category_id, category_title in
+                Tie.objects.filter(content_type=content_type).
+                    values_list('category_id', 'category__title').
+                    order_by('category__title').distinct()[:30]
         ]
 
         get_url = model.get_category_absolute_url
