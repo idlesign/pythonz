c31ec5ca2356e905af8e8cc0f68f6fcb3143dd15
idle sign <idlesign@yandex.ru>
2021-04-11 11:27:55 +0700

    Научились показывать «случайные» материалы на главной.

--- pythonz/apps/views/index.py
+++ pythonz/apps/views/index.py
@@ -1,11 +1,15 @@
+from datetime import timedelta
 from itertools import groupby
 from operator import attrgetter
+from typing import List
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.timezone import now
 from django.views.decorators.cache import cache_page
 from django.views.decorators.csrf import csrf_protect
 
+from ..generics.models import RealmBaseModel
 from ..generics.views import HttpRequest
 from ..models import ExternalResource
 
@@ -27,6 +31,9 @@ def index(request: HttpRequest) -> HttpResponse:
     max_items = 6
     min_local = 2
 
+    dt_stale_featured = now() - timedelta(days=7)
+    """Дата, начиная с которой следует считать материал устаревшим."""
+
     for name, realm in realms_registry.items():
 
         if not realm.show_on_main:
@@ -38,10 +45,20 @@ def index(request: HttpRequest) -> HttpResponse:
         if count_local < min_local:
             count_local = min_local
 
-        items = list(realm.model.get_actual()[:count_local])
+        actual = realm.model.get_actual()
+        items: List[RealmBaseModel] = list(actual[:count_local])
         items.extend(realm_externals[:max_items - count_local])
 
+        featured = None
+        if items:
+            featured = items[0]
+            if (featured.time_modified or featured.time_created) < dt_stale_featured:
+                # Объект устарел, покажем что-нибудь случайное.
+                # ? ведёт себя сносно, пока таблица влезает в память.
+                featured = actual.filter(id__lt=featured.id).order_by('?').first() or featured
+
         realms_data.append({
+            'featured': featured,
             'cls': realm,
             'items': items,
         })
