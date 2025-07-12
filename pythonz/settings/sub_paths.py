c2e987d6100c41e9017ec504068820a4f88e2a3a
idle sign <idlesign@yandex.ru>
2025-07-12 18:15:08 +0700

    Корректируем обнаружение LOCAL_RUN

--- pythonz/settings/sub_paths.py
+++ pythonz/settings/sub_paths.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).absolute().parent.parent
@@ -6,7 +7,7 @@ PROJECT_NAME = 'pythonz'
 PROJECT_DOMAIN = 'pythonz.net'
 
 PROJECT_DIR_STATE_LOCAL = BASE_DIR.parent / 'state'
-LOCAL_RUN = PROJECT_DIR_STATE_LOCAL.exists()
+LOCAL_RUN = PROJECT_DIR_STATE_LOCAL.exists() or os.environ.get('PYTEST_VERSION')
 
 if LOCAL_RUN:
     PROJECT_DIR_APP = BASE_DIR
